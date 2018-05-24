2eb39fde5518bbcabfeb685ca22cb4ea67227a0a
DayLightUA <sosigi@gmail.com>
2018-05-24 16:01:08 +0300

    Start

--- src/GUIclasses/GameGUI.java
+++ src/GUIclasses/GameGUI.java
@@ -1,10 +1,21 @@
 package GUIclasses;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by DayLightUA on 24.05.2018.
  */
 public class GameGUI {
     private static GameGUI instanceOfGame;
+
+    private JFrame mainFrame;
+    private JPanel mainPanel;
+    private JPanel topMenuPanel;
+    private JPanel characterPanel;
+    private JPanel enemysListPanel;
+    private JPanel choosedEnemyPanel;
+    private JPanel chatPanel;
 
     private GameGUI(){}
 
@@ -14,8 +25,28 @@ public class GameGUI {
     }
 
     public void go() {
+        initGUI();
+
+
     }
 
-    public void close() {
+    private void initGUI() {
+        mainFrame = new JFrame("SwingCombats");
+        mainPanel = new JPanel(new BorderLayout());
+        topMenuPanel = new JPanel();
+        characterPanel = new JPanel();
+        enemysListPanel = new JPanel();
+        choosedEnemyPanel = new JPanel();
+        chatPanel = new JPanel();
+
+        mainPanel.add(topMenuPanel, BorderLayout.NORTH);
+        mainPanel.add(characterPanel, BorderLayout.WEST);
+        mainPanel.add(enemysListPanel, BorderLayout.CENTER);
+        mainPanel.add(choosedEnemyPanel, BorderLayout.EAST);
+        mainPanel.add(chatPanel, BorderLayout.SOUTH);
+
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setSize(600, 600);
+        mainFrame.setVisible(true);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,5 @@ public class Main {
     public static void main(String[] args) {
         GameGUI gui = GameGUI.getInstance();
         gui.go();
-        gui.close();
     }
 }
